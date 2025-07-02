c2948887528d0b432adac864c2299883e5bda197
Amjad Almutairi <121881067+amjadAlmutairi11@users.noreply.github.com>
2025-07-02 10:38:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const toggleBtn = document.getElementById("toggle-theme");
 
 function setThemeIcon() {
-  const isDark = document.documentElement.hasAttribute("data-theme");
+  const isDark = document.documentElement.getAttribute("data-theme") === "dark";
   toggleBtn.textContent = isDark ? "🌞" : "🌙";
 }
 
 // Toggle theme and icon
-toggleBtn.addEvent
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    document.documentElement.removeAttribute("data-theme");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+  setThemeIcon();
+});
+
+// On first load, set correct icon
+setThemeIcon();
